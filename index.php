83cd8a591136e5c362fb9c4991acebc467f9805e
Anas95212 <anas.tahri9513@gmail.com>
2024-03-11 15:05:08 +0100

    Update index

--- index.php
+++ index.php
@@ -13,6 +13,43 @@ include ($Path_ref."inc/header.php");
     <p>Laissez-vous guider par vos sens et trouvez le gâteau parfait pour célébrer vos moments les plus précieux.</p>
 </div>
 
+<?php
+
+$jsonData = file_get_contents('data.json');
+
+$data = json_decode($jsonData, true);
+
+
+foreach ($data as $categoryName => $products) {
+    echo "<h2>" . htmlspecialchars($categoryName) . "</h2>";
+    echo "<div class='accueil-products-grid'>"; 
+
+    foreach ($products as $product) {
+        echo "<div class='accueil-card-product'>";
+        
+        echo "<img class='accueil-image-product' src='" . htmlspecialchars($product['image_link']) . "' alt='" . htmlspecialchars($product['name']) . "'>";
+        
+        echo "<h3 class='accueil-name-product'>" . htmlspecialchars($product['name']) . "</h3>";
+        
+        echo "<p class='accueil-description-product'>" . htmlspecialchars($product['description']) . "</p>";
+       
+        echo "<p class='accueil-price-product'>" . htmlspecialchars(current($product['prices'])) . "€</p>";
+
+        
+        echo "<div class='quantity-container'>";
+        echo "<label for='quantity'>Quantité: </label>";
+        echo "<input type='number' id='quantity' name='quantity' value='1' min='1' class='quantity-input'>";
+        echo "</div>";
+
+        
+        echo "<button class='accueil-add-to-cart-btn'>Ajouter au panier</button>";
+        echo "</div>"; 
+    }
+
+    echo "</div>"; 
+}
+?>
+
 
 <?php
 
