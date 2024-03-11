5a5d1f501d0bfee2e54de2ea49525cf5910f271c
ImTheNit <quentingag15@gmail.com>
2024-03-11 13:31:58 +0100

    Modif mineurs

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ include ("inc/header.php");
 include ("inc/footer.php");
 ?>
 <body>
+    <div id="div0" color="red">
+        <br> Ceci est le div 1
+    </div>
+    <div id="div1">
+        
+    </div>
+    <div id="div2">
     
+    </div>
+    <!--script src="/DevWeb/js/Accueil.js"></script-->
 </body>
 </html>
